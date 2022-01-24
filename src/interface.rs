409cc9f391b52ab117e69e904062ac1674440f07
xRetart <richard.network96@gmail.com>
2022-01-24 19:53:59 +0100

    reset

--- src/interface.rs
+++
@@ -1,9 +0,0 @@
-pub fn issue_error(msg: &str)
-{
-	issue("error", msg)
-}
-fn issue(lvl: &str, msg: &str)
-{
-	const PROGRAM_NAME: &str = "as-root";
-	eprintln!("[{}] {}: {}", PROGRAM_NAME, lvl, msg);
-}
