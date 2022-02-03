8bfee8e5b61721e3890bbe3d35945b597a74713e
xRetart <richard.network96@gmail.com>
2022-02-03 17:19:04 +0100

    complete rewrite

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,22 @@
-#![feature(termination_trait_lib)]
-
-mod interface;
-mod log;
+mod error;
 mod run;
+mod args;
 
-use std::process::ExitCode;
+use error::Error;
 
-// top most level main function used to safely exit with code
-fn main() -> ExitCode {
+
+fn main() {
     use std::process::exit;
 
-    exit(status_main());
+    exit(code_main())
 }
-
-// 2nd most top level main evaluating all errors
-fn status_main() -> i32 {
-    use {run::run_privileged, interface::parse_args};
-
-    let command = parse_args();
-
-    match run_privileged(command.program, command.args) {
-        Ok(status) => status.code().unwrap_or(0),
-        Err(_) => { log::error("failed to run command"); 1 },
-    }
+fn code_main() -> i32 {
+    result_main()
+        .unwrap_or_else(|err| { err.report(); 1 })
 }
+fn result_main() -> Result<i32, Error> {
+    use run::run_privileged;
 
-// lowest level main function doing all actual processing and returning an error
-fn real_main() -> Option<MainError> {
+    args::parse()
+        .and_then(run_privileged)
 }
--- src/run.rs
+++ src/run.rs
@@ -1,17 +1,15 @@
-use std::{ffi::OsStr, io, process::ExitStatus};
+use {std::process::Command, crate::error::Error};
 
-pub fn run_privileged<P, A>(program: P, args: A) -> io::Result<ExitStatus>
-where
-    P: AsRef<OsStr>,
-    A: IntoIterator<Item = P>,
-{
-    use std::{process::Command, os::unix::prelude::CommandExt};
+pub fn run_privileged(mut cmd: Command) -> Result<i32, Error> {
+    use std::os::unix::prelude::CommandExt;
+
     const ROOT: u32 = 0;
 
-    Command::new(program.as_ref())
-        .args(args)
+    cmd
         .uid(ROOT)
         .gid(ROOT)
-        .spawn()?
-        .wait()
+        .spawn()
+        .and_then(|mut child| child.wait())
+        .map_err(Error::Io)
+        .and_then(|status| status.code().ok_or(Error::NoExitCode))
 }
